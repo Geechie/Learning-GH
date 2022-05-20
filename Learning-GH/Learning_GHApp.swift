f587ed46cf5594c710f2e5df743d08aa780d34af
Geechie <68406864+Geechie@users.noreply.github.com>
2022-05-20 16:19:04 -0500

    Initial Commit

---
+++ Learning-GH/Learning_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GHApp.swift
+//  Learning-GH
+//
+//  Created by Brandon Taylor on 5/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
